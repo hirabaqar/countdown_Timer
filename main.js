c346932a9a53d5a25dc86b611e86ea6cd0fa73f6
hira baqar <148424134+hirabaqar@users.noreply.github.com>
2024-02-25 01:12:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+import { resolve } from "path";
+console.log("Welcome to Timer Project by HIRA BAQAR");
+// timer bana hai hour mintues & seconds//
+setTimeout(() => {
+    console.log("hours 5:");
+    setTimeout(() => {
+        console.log("mintues 25:");
+        setTimeout(() => {
+            console.log("seconds 20");
+        }, 5000);
+    }, 5000);
+}, 5000);
+//try it//
+resolve;
+setTimeout(() => {
+    console.log("Result");
+    resolve("hussain");
+}, 2000);
+setTimeout(() => {
+    console.log("Reject");
+    resolve("farah");
+}, 3000);
+// // try in 1 line execute//
+// setTimeout(() => {console.log('hours5:'; 'mintues 25:';'seconds 20')},5000)
+// console.log('Welcome to Timer Project')
+// let timer= await inquirer.prompt({
+// })
